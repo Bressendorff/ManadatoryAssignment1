2e9daa8989117e2791eb952b1a097ab594944481
Bressendorff <95355589+Bressendorff@users.noreply.github.com>
2022-09-30 13:29:24 +0200

    Add project files.

---
+++ ManadatoryAssignment1/FootballPlayer.cs
@@ -0,0 +1,57 @@
+﻿namespace ManadatoryAssignment1
+{
+    public class FootballPlayer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int ShirtNumber { get; set; }
+
+        public void Validate()
+        {
+            ValidateName();
+            ValidateAge();
+            ValidateShirtNumber();
+        }
+
+        public void ValidateName()
+        {
+            if (Name == null)
+            {
+                throw new ArgumentNullException("Name cannot be null");
+            }
+
+            if (string.IsNullOrWhiteSpace(Name)) throw new ArgumentException("Cannot be blank");
+            if (Name.Length < 2)
+            {
+                throw new ArgumentOutOfRangeException("Name must be at least 2 characters");
+            }   
+        }
+
+        public void ValidateAge()
+        {
+            if (Age < 2)
+            {
+                throw new ArgumentOutOfRangeException("Age must be over 1");
+            }
+        }
+
+        public void ValidateShirtNumber()
+        {
+            if (ShirtNumber > 99)
+            {
+                throw new ArgumentOutOfRangeException("Shirtnumber cannot be over 99");
+            }
+            if (ShirtNumber < 1)
+            {
+                throw new ArgumentOutOfRangeException("Shirtnumber must be at least 1");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(Name)}: {Name}, {nameof(Age)}: {Age}, {nameof(ShirtNumber)}: {ShirtNumber}";
+            
+        }
+    }
+}
---
+++ ManadatoryAssignment1Tests/FootballPlayerTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ManadatoryAssignment1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManadatoryAssignment1.Tests
+{
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+        private FootballPlayer correctFootballPlayer = new FootballPlayer() {Id = 1, Name = "Frederik", Age = 23, ShirtNumber = 12};
+        private FootballPlayer blankName = new FootballPlayer() {Id = 2, Name = "    ", Age = 23, ShirtNumber = 12};
+        private FootballPlayer nullName = new FootballPlayer() {Id = 3, Age = 23, ShirtNumber = 12};
+        private FootballPlayer playerTooYoung = new FootballPlayer() {Id = 5, Name = "Frederik", Age = 1, ShirtNumber = 12};
+        private FootballPlayer shirtTooLow = new FootballPlayer() {Id = 7, Name = "Frederik", Age = 23, ShirtNumber = 0};
+        private FootballPlayer shirtLowerLimit = new FootballPlayer() {Id = 8, Name = "Frederik", Age = 23, ShirtNumber = 1};
+        private FootballPlayer shirtUpperLimit = new FootballPlayer() {Id = 9, Name = "Frederik", Age = 23, ShirtNumber = 99};
+        private FootballPlayer shirtTooHigh = new FootballPlayer() {Id = 10, Name = "Frederik", Age = 23, ShirtNumber = 100};
+        private FootballPlayer tooShortName = new FootballPlayer() { Id = 11, Name = "a", Age = 23, ShirtNumber = 12 };
+        private FootballPlayer nameLimit = new FootballPlayer() { Id = 12, Name = "Bo", Age = 23, ShirtNumber = 12 };
+        private FootballPlayer ageLimit = new FootballPlayer() { Id = 13, Name = "Bo", Age = 2, ShirtNumber = 12 };
+
+
+        [TestMethod()]
+        public void ValidateTest()
+        {
+            correctFootballPlayer.Validate();
+            ValidateAgeTest();
+            ValidateNameTest();
+            ValidateShirtNumberTest();
+        }
+
+        [TestMethod()]
+        public void ValidateNameTest()
+        {
+            Assert.AreEqual("Frederik", correctFootballPlayer.Name);
+            Assert.ThrowsException<ArgumentNullException>(() => nullName.ValidateName());
+            Assert.ThrowsException<ArgumentException>(() => blankName.ValidateName()); 
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => tooShortName.ValidateName()); 
+            nameLimit.ValidateName();
+        }
+
+        [TestMethod()]
+        public void ValidateAgeTest()
+        {
+            correctFootballPlayer.ValidateAge();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => playerTooYoung.ValidateAge());
+            ageLimit.ValidateAge();
+            
+        }
+
+        [TestMethod()]
+        public void ValidateShirtNumberTest()
+        {
+            correctFootballPlayer.ValidateShirtNumber();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => shirtTooLow.ValidateShirtNumber());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => shirtTooHigh.ValidateShirtNumber());
+            shirtLowerLimit.ValidateShirtNumber();
+            shirtUpperLimit.ValidateShirtNumber();
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Assert.AreEqual("Id: 1, Name: Frederik, Age: 23, ShirtNumber: 12", correctFootballPlayer.ToString());
+        }
+    }
+}
